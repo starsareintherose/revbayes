f96a4bd68fb47b5aec25a28fb7d2e3d5f2e69aee
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-11-24 21:16:32 -0500

    First attempt at a generic function.

---
+++ src/core/functions/GenericFunction.h
@@ -0,0 +1,89 @@
+#ifndef GenericFunction_H
+#define GenericFunction_H
+
+#include "TypedDagNode.h"
+#include <boost/mp11.hpp>
+
+// The best implementation really wants C++14, so that we can use
+// boost::mp11::tuple_for_each( [&](const auto& p) { ...code...}, tuple);
+
+// We also need boost::mp11, which requires either Boost >= 1.68 OR
+// splitting out the Boost library.
+
+namespace RevBayesCore
+{
+    class DagNode;
+    template <class valueType> class TypedDagNode;
+
+    template <class R, class ...Args>
+    class GenericFunction : public TypedFunction<R>
+    {
+        // This keeps the types, but is harder to iterate over.
+        std::tuple<const TypedDagNode<Args>*...>  arguments;
+
+        // This is easier to iterate, but loses the types.
+        std::vector<const DagNode*> arguments2;
+
+        // The function we are wrapping.
+        R (*func)(Args...);
+
+        // A hand-coded lambda function with variable-type argument ( need C++14 to avoid this. )
+        struct getValue
+        {
+            template<typename T>
+            const T& operator()(const TypedDagNode<T>* node) const
+            {
+                return node->getValue();
+            }
+        };
+
+    public:
+
+        GenericFunction(R (*f)(Args...), const TypedDagNode<Args>*... args):
+            TypedFunction<R>(new R),
+            arguments({args...}),
+            arguments2({args...}),
+            func(f)
+        {
+            for(auto& arg: arguments2)
+                TypedFunction<R>::addParameter(arg);
+        }
+
+        GenericFunction<R, Args...>*  clone(void) const
+        {
+            return new GenericFunction<R, Args...>(*this);
+        }
+
+        void update(void)
+        {
+            using namespace boost::mp11;
+
+            auto values = tuple_transform(getValue(), arguments);
+            *TypedFunction<R>::value = tuple_apply(*func, values);
+        }
+
+    protected:
+        void swapParameterInternal(const DagNode *oldP, const DagNode *newP)
+        {
+            for(auto& arg: arguments2)
+                if (arg == oldP)
+                    arg = newP;
+        }
+
+    };
+
+    template <class R, class ...Args>
+    GenericFunction<R,Args...> generic_function(R (*f)(Args...), const TypedDagNode<Args>*... args)
+    {
+        return GenericFunction<R,Args...>(f, args...);
+    }
+
+    template <class R, class ...Args>
+    GenericFunction<R,Args...>* generic_function_ptr(R (*f)(Args...), const TypedDagNode<Args>*... args)
+    {
+        return new GenericFunction<R,Args...>(f, args...);
+    }
+}
+
+
+#endif
