a4c8f7714a062b18f199b52e2c682415f553504d
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-12 09:40:49 -0400

    Remove RbFileManager from Mcmc.cpp

--- src/core/analysis/mcmc/Mcmc.cpp
+++ src/core/analysis/mcmc/Mcmc.cpp
@@ -697,13 +697,11 @@ void Mcmc::initializeSamplerFromCheckpoint( void )
     std::vector<std::string> parameter_names;
     std::vector<std::string> parameter_values;
     
-    
     // check that the file/path name has been correctly specified
-    RevBayesCore::RbFileManager fm( checkpoint_file_name.string() );
-    if ( !fm.testFile() || !fm.testDirectory() )
+    if ( not is_regular_file( checkpoint_file_name) )
     {
         std::string errorStr = "";
-        fm.formatError( errorStr );
+        formatError( checkpoint_file_name, errorStr );
         throw( RbException(errorStr) );
     }
     
